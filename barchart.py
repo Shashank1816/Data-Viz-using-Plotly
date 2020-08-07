7e5f479f971cc092c6f024286f2c751f1a88dc0d
Shashank Verma <shashankverma1816@gmail.com>
2020-08-07 05:52:08 +0530

    made changes in barchart, converted it to nested

--- barchart.py
+++ barchart.py
@@ -5,12 +5,24 @@ import plotly.graph_objs as go
 
 df=pd.read_csv('Data/2018WinterOlympics.csv')
 print(df.head())
-
-data=[go.Bar(x=df['NOC'],
-            y=df['Total'],
-            )]
- 
-layout=go.Layout(title="Simple Bar Graph")
+trace1=go.Bar(x=df['NOC'],
+            y=df['Gold'],
+            name='Gold',
+            marker=dict(color='#FFD700')
+            )
+trace2=go.Bar(x=df['NOC'],
+            y=df['Silver'],
+            name='Silver',
+            marker=dict(color='gray')
+            )
+trace3=go.Bar(x=df['NOC'],
+            y=df['Bronze'],
+            name='Bronze',
+            marker=dict(color='#CD7F32')
+            )
+                                    
+data=[trace1,trace2,trace3]
+layout=go.Layout(title="Nested Bar Graph")
 fig=go.Figure(data=data, layout=layout)
 
 pyo.plot(fig,filename='Barchart.html')
